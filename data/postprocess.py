d7bd11e15e2445edc898543ab4ddb860b8e2bd51
Yoeri Poels <yoeri.poels@hotmail.com>
2021-07-01 11:43:06 +0200

    added integer images -> float32 images to parse_data_x()

--- data/postprocess.py
+++ data/postprocess.py
@@ -17,6 +17,8 @@ def parse_data_x(data, h=None, w=None, d=None):
         else:
             d = 1
     data = data.reshape(data.shape[0], h, w, d)
+    if issubclass(data.dtype.type, np.integer):
+        data = data / 255
     data = data.astype('float32')
     return data
 
@@ -89,7 +91,7 @@ def create_data(data_dir, data_names, filename='output', w=32, h=32, n=10000, da
     return return_val if len(return_val) > 1 else return_val[0]  # if we only have x, don't wrap in list
 
 
-def create_adverse_data(data_a, data_b, n=None):
+def create_adverse_data(data_a, data_b, n=None, adverse_use_same=True):
     """Create pairs consisting of two randomly selected 'change-pair' images
     """
     if n is None:
@@ -100,8 +102,12 @@ def create_adverse_data(data_a, data_b, n=None):
     adverse_pairs_b = np.zeros(data_shape)
     idx_ord_a = np.random.permutation(n)
     idx_ord_b = np.random.permutation(n)
+    if adverse_use_same:
+        pick_random_choice = [True, False]
+    else:
+        pick_random_choice = [True]  # we only pick different-image pairs
     for i in range(n):
-        if random.choice([True, False]):  # pick a pair of two random elements, not matching idx
+        if random.choice(pick_random_choice):  # pick a pair of two random elements, not matching idx
             adverse_pairs_a[i][:] = data_a[idx_ord_a[i]][:]
             idx_b = idx_ord_b[i]
             if random.choice([True, False]):  # randomly pick from either before or after
@@ -118,17 +124,27 @@ def create_adverse_data(data_a, data_b, n=None):
     return adverse_pairs_a, adverse_pairs_b
 
 
-def create_change_pairs(cpair_p_a, cpair_p_b, cpair_n_a, cpair_n_b):
-    """From positive change pairs (size = N/2) and negative change pairs (size = N/4), create full change pair
-    arrays / labels.
+def create_change_pairs(cpair_p_a, cpair_p_b, cpair_n_a=None, cpair_n_b=None, adverse_use_same=True,
+                        generate_adverse=True):
+    """From positive change pairs (size = N/2) and (optionally) negative change pairs (generally size = N/4, or N/2 if
+    we do not generate extra adverse pairs), create full change pair arrays / labels.
     """
-
-    # first, create extra negative pairs by also combining random images as bad pairs
-    # (opposed to only pairs consisting of images with 2+ lines changed)
-    cpair_adv_a, cpair_adv_b = create_adverse_data(cpair_p_a, cpair_p_b)
-    n_negative = cpair_n_a.shape[0]
-    cpair_adv_a[0:n_negative] = cpair_n_a[:]
-    cpair_adv_b[0:n_negative] = cpair_n_b[:]
+    assert cpair_p_a.shape[0] == cpair_p_b.shape[0]
+    if generate_adverse:
+        # first, create extra negative pairs by also combining random images as bad pairs
+        # (opposed to only pairs consisting of images with 2+ factors changed)
+        cpair_adv_a, cpair_adv_b = create_adverse_data(cpair_p_a, cpair_p_b, adverse_use_same=adverse_use_same)
+    else:
+        # do not create extra negative pairs, only use those supplied
+        # assert these have the correct dimension
+        assert cpair_n_a is not None and cpair_n_b is not None
+        assert cpair_n_a.shape[0] == cpair_n_b.shape[0] == cpair_p_a.shape[0]
+        cpair_adv_a = cpair_n_a
+        cpair_adv_b = cpair_n_b
+    if cpair_n_a is not None and cpair_n_b is not None:
+        n_negative = cpair_n_a.shape[0]
+        cpair_adv_a[0:n_negative] = cpair_n_a[:]
+        cpair_adv_b[0:n_negative] = cpair_n_b[:]
 
     # now that we have equal sized positive and negative pairs, construct final data
     n_half, h, w, d = cpair_p_a.shape[0:4]
@@ -154,7 +170,7 @@ def create_change_pairs(cpair_p_a, cpair_p_b, cpair_n_a, cpair_n_b):
 
 def create_feature_pairs(x, y, y_feature, n_pair=None):
     """Creates pairs of datapoints where each datapoint share at least one feature, with this shared feature
-    labeled by y_shared.
+    labeled by y_shared. Pick a similar number of 'positive' and 'negative' features to share.
     """
     #######
     # First, create lists per feature, where each such list contains datapoints with/without that feature
@@ -162,7 +178,72 @@ def create_feature_pairs(x, y, y_feature, n_pair=None):
     feature_to_data = []
     nfeature_to_data = []
     idx_to_feature = []
-    num_feature = 8
+    num_feature = len(y_feature)
+    for i in range(num_feature):
+        feature_to_data.append([])
+        nfeature_to_data.append([])
+
+    n = x.shape[0]
+    for i in range(n):
+        features = []
+        non_features = []
+        for j in range(num_feature):
+            if y_feature[j][i][1] == 1:  # feature exists
+                features.append(j)
+            else:
+                non_features.append(j)
+        # positive feature, y_f=1
+        to_add = random.choice(features)
+        feature_to_data[to_add].append((x[i], y[i]))
+        # negative feature, y_f=0
+        to_add = random.choice(non_features)
+        nfeature_to_data[to_add].append((x[i], y[i]))
+        idx_to_feature.append(to_add)
+
+    #######
+    # From these feature lists, create pairs of data with (at least) 1 feature in common, and label this feature
+    #######
+    if n_pair is None:
+        n_pair = n
+    _, h, w = x.shape[0:3]
+    num_class_y = y.shape[1]
+    x_fpair_a = np.zeros((n_pair, h, w, 1))
+    x_fpair_b = np.zeros((n_pair, h, w, 1))
+    y_fpair = np.zeros((n_pair, 1))
+    y_fpair_a = np.zeros((n_pair, num_class_y))
+    y_fpair_b = np.zeros((n_pair, num_class_y))
+    for i in range(n_pair):
+        f = np.random.randint(num_feature)
+        if random.choice([True, False]):
+            # pick from positive feature
+            idx_a, idx_b = np.random.randint(len(feature_to_data[f]), size=2)
+            x_fpair_a[i][:] = feature_to_data[f][idx_a][0][:]
+            x_fpair_b[i][:] = feature_to_data[f][idx_b][0][:]
+            y_fpair_a[i][:] = feature_to_data[f][idx_a][1][:]
+            y_fpair_b[i][:] = feature_to_data[f][idx_b][1][:]
+        else:
+            # pick from negative feature
+            idx_a, idx_b = np.random.randint(len(nfeature_to_data[f]), size=2)
+            x_fpair_a[i][:] = nfeature_to_data[f][idx_a][0][:]
+            x_fpair_b[i][:] = nfeature_to_data[f][idx_b][0][:]
+            y_fpair_a[i][:] = nfeature_to_data[f][idx_a][1][:]
+            y_fpair_b[i][:] = nfeature_to_data[f][idx_b][1][:]
+        y_fpair[i][0] = f
+    y_fpair = y_fpair.astype('float32')
+    return x_fpair_a, x_fpair_b, y_fpair, y_fpair_a, y_fpair_b
+
+
+def create_feature_pairs_cat(x, y, y_feature, n_pair=None):
+    """Creates pairs of datapoints where each datapoint share at least one feature, with this shared feature
+    labeled by y_shared. Features are considered categorical, i.e., pick evenly from all shared features.
+    """
+    #######
+    # First, create lists per feature, where each such list contains datapoints with/without that feature
+    #######
+    feature_to_data = []
+    nfeature_to_data = []
+    idx_to_feature = []
+    num_feature = len(y_feature)
     for i in range(num_feature):
         feature_to_data.append([])
         nfeature_to_data.append([])
